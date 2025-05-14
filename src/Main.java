66a2b061019761ad90ef2d7cd6ebd351dba728e2
gouhara takuya <241126@std.hi-joho.ac.jp>
2025-05-14 09:49:20 +0900

    GitHubにアップロードするテスト

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main (String[] args){
-        System.out.println("合原　拓弥");
+        System.out.println("合原");
     }
 }
---
+++ src/hennkou.java
@@ -0,0 +1,6 @@
+public class hennkou {
+    public static void main (String[] args){
+        System.out.println("合原");
+    }
+}
+
