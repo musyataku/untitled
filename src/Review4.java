8714ac3463f35fd89ef582293fafd6c5a39475a2
gouhara takuya <241126@std.hi-joho.ac.jp>
2025-05-16 12:19:09 +0900

    復習問題９個目まで

---
+++ src/Review4.java
@@ -0,0 +1,15 @@
+public class Review4 {
+    public static void main(String[] args) {
+        int number = 5;
+        String str = 88 + "88";
+        final double PI = 3.141592;
+        float circ = 3.14f;
+        long a = 1000000000;//大きすぎるので０をけしました。
+        boolean flg = true;
+        byte b = 35;
+        int num = 0;//何をさせたいのかがわかりませんでした
+        char c = 'a';
+    }
+}
+
+
---
+++ src/Review9.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Review9 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("名前を入力してください>");
+        String inputName = br.readLine();
+        System.out.print("年齢を入力してください>");
+        int age = Integer.parseInt(br.readLine());
+        System.out.println(age + "歳の" + inputName + "さん。" + "\n" + "こんにちは。");
+    }
+}
