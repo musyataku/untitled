707f74d3daada9a0225a202b328a1d85aa058996
gouhara takuya <241126@std.hi-joho.ac.jp>
2025-06-02 12:30:30 +0900

    25~27

---
+++ src/Review/review2/Review26.java
@@ -0,0 +1,16 @@
+package Review.review2;
+
+public class Review26 {
+    public static void main(String[] args) {
+        int [] numArray = new int [] {1,2,3,4,5,6,7,8,9,10};
+        int target = 8;
+        int count = 0;
+        for (int i = 0; i < numArray.length; i++) {
+            if (numArray[i] == target) {
+                count++;
+            }
+        }
+        System.out.println("探索した値" + target);
+        System.out.println("一致した数" + count);
+    }
+}
